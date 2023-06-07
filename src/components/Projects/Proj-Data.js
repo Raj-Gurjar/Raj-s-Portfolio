368240e5b6d3dbcf364fb1ad3c633c77aa4a53b5
Raj-Gurjar <20ai1045@mitsgwl.ac.in>
2023-06-07 22:31:07 +0530

    add-proj-filter

---
+++ src/components/Projects/Proj-Data.js
@@ -0,0 +1,40 @@
+const ProjData = [
+
+
+    {
+        id:1,
+        title:"Portfolio Website",
+        subTitle:"Reactjs",
+        image:"imgs/web.png",
+        category:"WebDev",
+        discription:"This is a portfolio website made using Reactjs.",
+        liveLink: "https://leetcode.com/Raj_Gurjar/",
+        gitLink: "https://github.com/Raj-Gurjar"
+    },
+    
+    {
+        id:2,
+        title:"Movie System",
+        subTitle:"NLP",
+        image:"imgs/Ai.png",
+        category:"AI",
+        discription:"This is a Movie Recommendation System made using NLP.",
+        liveLink: "https://leetcode.com/Raj_Gurjar/",
+        gitLink: "https://github.com/Raj-Gurjar"
+    },
+
+    {
+        id:3,
+        title:"Dsa Profiles",
+        subTitle:"DSA",
+        image:"imgs/DSA.png",
+        category:"DSA",
+        discription:"These are my DSA profiles.",
+        liveLink: "https://leetcode.com/Raj_Gurjar/",
+        gitLink: "https://github.com/Raj-Gurjar"
+    }
+
+    
+];
+
+export default ProjData;
--- src/components/Projects/ProjectCom.js
+++ src/components/Projects/ProjectCom.js
@@ -1,73 +1,96 @@
-import "./ProjectCom.scss";
-import projimg from "../../assets/Project-Imgs/gfgXleet.jpg";
-import { FaLinkedin, FaGit } from 'react-icons/fa';
-
-
-import React from 'react';
+import React, { useState } from 'react';
+import './ProjectCom.scss';
+import ProjData from './Proj-Data';
 
 const ProjectCom = () => {
+    const [items, setItems] = useState(ProjData);
+    const [activeFilter, setActiveFilter] = useState('All');
 
+    const filterItem = (categItem) => {
+        const updatedItems = categItem === 'All' ? ProjData : ProjData.filter((curElem) => curElem.category === categItem);
+        setItems(updatedItems);
+        setActiveFilter(categItem);
+    };
+
+    const [expandedCardId, setExpandedCardId] = useState(null);
+    const toggleDetails = (cardId) => {
+        setExpandedCardId(cardId === expandedCardId ? null : cardId);
+    };
 
     return (
-
-        <section class="port_cls section_padding" id="Portfolio">
-            <div class="container">
-
-                <div class="row justify_content_cntr">
-                    <div class="section_title txt_align">
-                        <h3 class="main_title">Projects</h3>
-                        <h5 class="sub_title">Works I Did</h5>
-                        <div class="line" />
+        <section className="port_cls section_padding">
+            <div className="container">
+                <div className="row justify_content_cntr">
+                    <div className="section_title txt_align">
+                        <h3 className="main_title">Projects</h3>
+                        <h5 className="sub_title">Works I Did</h5>
+                        <div className="line" />
                     </div>
                 </div>
 
+                <div className="row justify_content_cntr">
+                    <div className="port_filter">
+                        {['All', 'DSA', 'WebDev', 'AI', 'Others'].map((category) => (
+                            <button
+                                key={category}
+                                className={activeFilter === category ? 'active' : ''}
+                                onClick={() => filterItem(category)}
+                            >
+                                {category}
+                            </button>
+                        ))}
+                    </div>
+                </div>
 
+                <div className="row justify_content_cntr">
+                    {items.map((elem) => {
+                        const {
+                            id,
+                            title,
+                            subTitle,
+                            image,
+                            category,
+                            discription,
+                            liveLink,
+                            gitLink,
+                        } = elem;
 
-                <div class="row justify_content_cntr">
+                        const isExpanded = expandedCardId === id;
 
-                    {/* <!--? Item1 -s --> */}
-                    <div class="col-11">
-
-                        <div className="proj-box-1">
-                            <div className="port-img">
-
-                                <div className="proj-info">
-                                    <h5>
-                                        <a href="https://github.com/Raj-Gurjar">Portfolio Website</a>
-                                    </h5>
-                                    <span>Reactjs</span>
+                        return (
+                            <div className="card" key={id}>
+                                <div className="imgBox">
+                                    <div className="proj-title">
+                                        <h4>{title}</h4>
+                                        <h6>
+                                            {subTitle}
+                                            <span>
+                                                <button onClick={() => toggleDetails(id)}>
+                                                    {isExpanded ? 'Hide Info' : 'View Info'}
+                                                </button>
+                                            </span>
+                                        </h6>
+                                    </div>
+                                    <img src={image} alt="proj-img" />
+                                    <div className="btn-box">
+                                        <button>
+                                            <a href={gitLink}>View Code</a>
+                                        </button>
+                                        <button>
+                                            <a href={liveLink}>Live Demo</a>
+                                        </button>
+                                    </div>
                                 </div>
-
-                                <img src={projimg} alt="" />
-
+                                <div className={`details ${isExpanded ? 'show' : ''}`}>
+                                    <p>{discription}</p>
+                                </div>
                             </div>
-
-                        </div>
-                    </div>
-
-
-
-
-                    {/* <!--? item1 --> */}
-
-
-
-
-                    {/* <!--? port Item -e --> */}
-
-
-
-
-
-
-
+                        );
+                    })}
                 </div>
-
-
             </div>
-        </section >
-
-    )
-}
+        </section>
+    );
+};
 
 export default ProjectCom;
