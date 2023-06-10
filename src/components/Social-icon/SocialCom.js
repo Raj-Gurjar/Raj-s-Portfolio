0e79b7545f48ca75bd984146850b6aac5143d743
Raj-Gurjar <20ai1045@mitsgwl.ac.in>
2023-06-10 18:48:17 +0530

    Add-social-arrow

--- src/components/Social-icon/SocialCom.js
+++ src/components/Social-icon/SocialCom.js
@@ -1,38 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react';
 import './SocialCom.scss';
-
-import { FaLinkedinIn, FaGithub, FaInstagram,FaWhatsapp } from "react-icons/fa";
-
-
+import { FaLinkedinIn, FaGithub, FaInstagram, FaWhatsapp,FaComment } from "react-icons/fa";
+// import FontAwesomeIcon from '@fortawesome/react-fontawesome';
 
 const SocialCom = () => {
+    const [showIcons, setShowIcons] = useState(false);
+
+    const handleToggle = () => {
+        setShowIcons(!showIcons);
+    };
+
     return (
+        <div className={`social-icons ${showIcons ? 'show' : ''}`}>
+            <div className="social-toggle" onClick={handleToggle}>
+            </div>
 
-    
-        <social className="social-icons">
-
-
-                <div className="social_box">
-
-                    <a href="https://www.facebook.com/" rel="noreferrer" target="_blank"><FaLinkedinIn />
-                    </a>
-
-                    <a href="https://www.linkedin.com/" rel="noreferrer" target="_blank" ><FaGithub />
-                    </a>
-
-                    <a href="https://www.linkedin.com/" rel="noreferrer" target="_blank"><FaInstagram />
-                    </a>
-
-                    <a href="https://www.linkedin.com/" rel="noreferrer" target="_blank"><FaWhatsapp />
-                    </a>
-
-                </div>
-
-
-
-
-        </social>
-    )
-}
+            <div className="social-box">
+                <a href="https://www.facebook.com/" rel="noreferrer" target="_blank">
+                    <FaLinkedinIn />
+                </a>
+                <a href="https://www.linkedin.com/" rel="noreferrer" target="_blank">
+                    <FaGithub />
+                </a>
+                <a href="https://www.linkedin.com/" rel="noreferrer" target="_blank">
+                    <FaInstagram />
+                </a>
+                <a href="https://www.linkedin.com/" rel="noreferrer" target="_blank">
+                    <FaWhatsapp />
+                </a>
+            </div>
+        </div>
+    );
+};
 
 export default SocialCom;
